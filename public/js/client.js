26fd38efb0c468ab0e2c65029886cd66707b60d1
santio89 <santiolais1989@gmail.com>
2022-08-10 00:35:08 -0300

    small fix

--- public/js/client.js
+++ public/js/client.js
@@ -138,9 +138,9 @@ async function renderItems(items, logStatus) {
 function displayTable() {
     const table = document.querySelector(".productos__table")
     const noProd = document.querySelector(".productos__noProd")
-    if (table?.classList?.contains("v-none")) {
-        table.classList.remove("v-none")
-        noProd.classList.add("v-none")
+    if (table?.classList?.contains("d-none")) {
+        table.classList.remove("d-none")
+        noProd.classList.add("d-none")
     }
 }
 
@@ -254,18 +254,17 @@ socket.on("server:items-test", async items => {
         if (logStatus.status === 401) {
             return
         }
+
+        displayTable()
+        const mockData = await fetch("/api/productos-test")
+        const mockProducts = await mockData.json()
+
+        mockProducts.forEach(product => {
+            renderProducto(product)
+        })
     } catch (e) {
         console.log("error fetching login: ", e)
     }
-
-
-    displayTable()
-    const mockData = await fetch("/api/productos-test")
-    const mockProducts = await mockData.json()
-
-    mockProducts.forEach(product => {
-        renderProducto(product)
-    })
 })
 
 socket.on("server:producto", producto => {
